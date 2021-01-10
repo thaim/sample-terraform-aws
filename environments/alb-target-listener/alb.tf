d6f7fc04be2fc8b7282db2e09f96210929269831
thaim <thaim24@gmail.com>
2021-01-10 13:02:35 +0000

    initial commit for alb resources

---
+++ environments/alb-target-listener/alb.tf
@@ -0,0 +1,74 @@
+resource "aws_lb" "sample_alb" {
+  name               = "sample-alb"
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.allow_access_alb.id,
+    aws_security_group.allow_alb_access_ecs.id
+  ]
+  subnets         = [
+    data.aws_subnet.public_a.id,
+    data.aws_subnet.public_c.id,
+    data.aws_subnet.public_d.id
+  ]
+
+  enable_deletion_protection = false
+
+  tags = {
+    terraform = true
+  }
+}
+
+resource "aws_lb_target_group" "sample" {
+  name = "sample-alb-target-group"
+
+  port = 80
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id = var.vpc_id
+
+  health_check {
+    enabled = true
+
+    interval = 60
+  }
+
+  tags = {
+    terraform = true
+  }
+}
+
+resource "aws_lb_listener" "sample_alb_listener" {
+  load_balancer_arn = aws_lb.sample_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.sample.arn
+  }
+}
+
+resource "aws_security_group" "allow_access_alb" {
+  name = "AllowAccessALB"
+
+  vpc_id = var.vpc_id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+  }
+
+  tags = {
+    terraform = true
+  }
+}
---
+++ environments/alb-target-listener/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
